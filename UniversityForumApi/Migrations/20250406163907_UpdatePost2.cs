23d13a26104756aa6d2e2d1ee6226183e23de628
NightBlade <nguyenhakien8@gmail.com>
2025-04-07 00:18:57 +0700

    fixing

---
+++ UniversityForumApi/Migrations/20250406163907_UpdatePost2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UniversityForumApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePost2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsHidden",
+                table: "Posts",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsHidden",
+                table: "Posts");
+        }
+    }
+}
